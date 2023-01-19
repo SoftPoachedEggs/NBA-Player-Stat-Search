0d03286e17af2ec610ac39ed1e57da0a67124437
alexoserna <118146045+alexoserna@users.noreply.github.com>
2023-01-19 13:15:33 -0500

    added a function to pull awards and team name from wiki

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 /* I need to pull 1 image from the Wikipedia page for the player picture and 1 image for the player team banner */
-var player = "Kobe Bryant";
+var player = "Derrick Rose";
 var pImg = document.querySelector("img");
-var pName = document.getElementById("name");
-var pHeight = document.getElementById("height");
+var tName = document.getElementById("team-name");
+var highlights = document.getElementById("highlights");
 var pWeight = document.getElementById("weight");
 
 
@@ -54,73 +54,50 @@ var url = "https://en.wikipedia.org/w/api.php?" +
         var wikiHTML = data.parse.text["*"];
         // strips down html
         var plainWiki = wikiHTML.replace(/<(?:.|\n)*?>/gm, '');
-        /*
-        For Career Highlights
-        */
-        // saved string for the first index position
-        var highlights = 'Career highlights and awards';
-        var indexCareer = plainWiki.indexOf(highlights);
-        // string at the end of the career highlights and awards
-        var indexStat =plainWiki.indexOf('Stats');
-        // prints index position
-        console.log("starting index " + indexCareer);
-        console.log("ending index " + indexStat );
-        //saves variable for hightlights
-        var careerHighlights = plainWiki.slice( indexCareer + highlights.length, indexStat );
-        console.log(careerHighlights);
-        /*
-        For Team
-        */
-
-        var beginning = '&#32;';
-        var indexTeam = plainWiki.lastIndexOf(beginning);
-        var indexPosition = plainWiki.indexOf('Position');
-        var teamName = plainWiki.slice(indexTeam + beginning.length, indexPosition);
-        console.log(teamName);
-
-
-        // prints the wiki after being stripped
-        // console.log(plainWiki);      
+        // Parse text and return string
+        var accomplishment = parseText(plainWiki, 'highlights');
+        var teamName = parseText(plainWiki, 'team');
+        tName.innerHTML = teamName;
+        var accomplishments = accomplishment.split(')');
+        for ( i = 0; i < (accomplishments.length-1); i++ ) {
+          console.log(i)
+          // can choose to do a different type of element
+          var tag = document.createElement("p");
+          // replace .highlights with container that is to hold highlights
+          var container = document.querySelector(".highlights");
+          tag.className = 'highlight';
+          tag.textContent = accomplishments[i];
+          container.appendChild(tag);
+        }
+        
     })
     .catch(function(error){console.log(error);});
 
-
-async function funcName(pt) {
-    var url = "https://en.wikipedia.org/w/api.php?" +
-/* Sets search parameters for the api to target */
-    new URLSearchParams({
-        origin: "*",
-        action: "parse",
-        prop:"text",
-        page: pt,
-        format: "json",
-    });
-    const response = fetch(url);
-    var data = response.json();
-    var wikiHTML = data.parse.text["*"];
-    // strips down html
-    var plainWiki = wikiHTML.replace(/<(?:.|\n)*?>/gm, '');
+function parseText (text, term) {
+  // creates variables
+  var index1;
+  var index2;
+  var indexOne;
+  var indexTwo;
+  // Sorts through which term is used
+  if ( term == 'highlights' ) {
+    index1 = 'Career highlights and awards\n';
+    index2 = '\n\n';
+    indexOne = text.indexOf(index1);
+    indexTwo = text.indexOf(index2, indexOne);
+  } else if (term == 'team') {
+    index1 = '&#32;';
+    index2 = 'Position';
+    indexOne = text.lastIndexOf(index1);
+    indexTwo = text.indexOf(index2, indexOne);
+  } else {
+    console.log("Please insert proper parameters\n-------------------------------\nsearch---------------term\n-------------------------------\nplayer---------------highlights\nplayer---------------team\nteam-----------------colors");
+  }
+  // prints to console the index positions to check that everything is working properly
+  // console.log("starting index " + indexOne);
+  // console.log("ending index " + indexTwo );
+  // Splices plain text to obtain needed string
+  var returnString = text.slice(indexOne + index1.length, indexTwo);
+  console.log(returnString);
+  return returnString;
 }
-
-
-
-    var url = "https://en.wikipedia.org/w/api.php?" +
-/* Sets search parameters for the api to target */
-    new URLSearchParams({
-        origin: "*",
-        action: "parse",
-        prop:"text",
-        page: 'Golden State Warriors',
-        format: "json",
-    });
-    fetch(url)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data) {       
-        var wikiHTML = data.parse.text["*"];
-        // strips down html
-        var plainWiki = wikiHTML.replace(/<(?:.|\n)*?>/gm, '');
-        console.log(plainWiki);
-    })
-    .catch(function(error){console.log(error);});
